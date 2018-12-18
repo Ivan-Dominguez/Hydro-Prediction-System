ba0ac855f1ff7b492acf6128aa082b8409187c07
Ivan <ridoming@lakeheadu.ca>
2018-12-18 11:04:15 -0500

    Update decisionTree_fwts.R

--- training_data/Scaled Training Files/decisionTree_fwts.R
+++ training_data/Scaled Training Files/decisionTree_fwts.R
@@ -1,33 +1,26 @@
 library(rpart)
 library(caTools)
-library(plyr)
+library (dplyr)
 
-
-setwd("~/Google Drive/Degree Project/training_data/Scaled Training Files")
+setwd("~/Google Drive/Degree Project/Repository/Hydro-Prediction-System/training_data/Scaled Training Files")
 
 #load data
 x_vars <- read.csv("scaled_training_fwts_XVars.csv", header=TRUE, sep=",", na.strings=c("NA", "NULL"))
 y_var <- read.csv("scaled_training_fwts_YVar.csv", header=TRUE, sep=",", na.strings=c("NA", "NULL"))
 
-
 #merge files
 x_vars$y <- paste(y_var$V1)
 
 #split in training and test sets
-sets_x <- sample.split(x_vars, Splitratio = 0.8)
-sets_y <- sample.split(y_var, Splitratio = 0.8)
-
-train_x <- subset(x_vars, sets_x == TRUE)
-test_x <- subset(x_vars, sets_x == FALSE)
-
-train_y <- subset(y_var, sets_y == TRUE)
-test_y <- subset(y_vars, sets_y == FALSE)
-
+split <- sample.split(x_vars$y, SplitRatio = 0.8)
+trainning_set <- subset(x_vars, split == TRUE)
+test_set <- subset(x_vars, split == FALSE)
   
-
 #create tree
-tree <- rpart(Play ~.,
-              method="class", data = DTdata, minsplit = 1)
+tree <- rpart(y ~., data = x_vars)
+
+#prediction
+y_predict <- predict(tree, newdata = test_set)
 
 summary(tree)
 rpart.plot(tree)
