fe126ad52010164d5b5f4b545479dea539757098
Gavy Dong <jdong4@lakeheadu.ca>
2019-02-23 16:59:30 -0500

    Testing

--- Testing.R
+++ Testing.R
@@ -23,14 +23,18 @@ h2o.init()
 ##############################################33
 data <- read.csv("G:/degree project/trainingFile_fwts.csv", header=TRUE, sep=",", na.strings=c("NA", "NULL"),stringsAsFactors=FALSE)
 data$pres = as.numeric(data$pres)
+peak_times_list<-c(RFcoord_fwts_pred,XGcoord_pred,CUcoord_pred,DLcoord_pred,AVEcoord_pred,MEDIAN_pred,xcoord_test_pred)
+pred_times <-data.frame(matrix(nrow = 30, ncol = 8))
+columns = c("Date","xgboost", "cubist","deepLearning","rf", "avg", "median", "real_peak")
+colnames(pred_times) <- columns
 ######################################### set dates ############################################
-prediction_date_str<-"2018-03-19"
+prediction_date_str<-"2018-02-28"
 prediction_date <- as.Date(prediction_date_str)
 i=0
-while(i<5){
+while(i<9){
 prediction_date=prediction_date+1;
 day_before<-prediction_date - 1
-last_30days_date <- prediction_date - 365
+last_30days_date <- prediction_date - 60
 
 #dates to string
 day_before_str<-as.character.Date(day_before)
@@ -51,14 +55,17 @@ test_set_real<-data %>% filter(str_detect(datetime, toString(prediction_date)))
 test_set<-test_set_real[,c(-1,-2,-19,-20)]
 #########################################################
 model3<-cubist(x = training_set[-1], y = training_set$fwts, committees = 20, neighbors = 5)
-
-model4=h2o.deeplearning(y='fwts',
-                                  x=c('temp','dew','hum','wspd','vis','pres','mon','tue','wed','thu','fri','sat','sun','sint','cost'),
-                                  activation = 'Rectifier',
-                                  training_frame=as.h2o(training_set),
-                                  hidden=c(10,10),
-                                  epochs=100,
-                                  train_samples_per_iteration=-2
+scale_training_set<-training_set
+scale_training_set[1]<-scale(training_set[1])
+scale_scale=attr(scale(training_set[1]), 'scaled:scale')
+scale_center=attr(scale(training_set[1]), 'scaled:center')
+model4<-h2o.deeplearning(y='fwts',
+                        x=c('temp','dew','hum','wspd','vis','pres','mon','tue','wed','thu','fri','sat','sun','sint','cost'),
+                        activation = 'Rectifier',
+                        training_frame=as.h2o(as.matrix(scale_training_set)),
+                        hidden=c(10,10),
+                        epochs=100,
+                        train_samples_per_iteration=-2
 )
 
 variables=c('temp','dew','hum','wspd','vis','pres','mon','tue','wed','thu','fri','sat','sun','sint','cost')
@@ -70,10 +77,12 @@ model1<-h2o.randomForest(x=variables,
                          seed=1242525
 )
 model2 = xgboost(data = as.matrix(training_set[-1]), label = training_set$fwts,  nrounds = 100)
+mat<-xgb.importance (feature_names = colnames(training_set[-1]),model = model2)
 y_pred2 = predict(model2, newdata = as.matrix(test_set[-1]))
 y_pred1 = predict(model1, newdata = as.h2o(test_set[-1]))
 y_pred3 = predict(model3, (test_set[-1]))
 y_pred4 = predict(model4,newdata=as.h2o(test_set[-1]))
+y_pred4 = y_pred4*scale_scale+scale_center
 y_pred5 = (as.vector(y_pred1)+y_pred2+y_pred3+as.vector(y_pred4))/4
 #visualize
 
@@ -109,8 +118,22 @@ AVEcoord_pred <-
   test_set_real$datetime[which.max(as.vector(y_pred5)[144:288])+143]
 
 #Median of peak time
-MEDIAN_pred=median(c(AVEcoord_pred,RFcoord_fwts_pred,DLcoord_pred,XGcoord_pred,CUcoord_pred))
+MEDIAN_pred <- strptime(prediction_date, "%Y-%m-%d")-mean(difftime(
+  paste(prediction_date, "00:00:00", sep=" "),
+  c(RFcoord_fwts_pred,DLcoord_pred,XGcoord_pred,CUcoord_pred),
+  units = "secs"
+))
+MEDIAN_pred <-as.character(MEDIAN_pred)
+#store difference
 
+pred_times$xgboost[i]=XGcoord_pred
+pred_times$cubist[i]=CUcoord_pred
+pred_times$deepLearning[i]=DLcoord_pred
+pred_times$rf[i]=RFcoord_fwts_pred
+pred_times$avg[i]=AVEcoord_pred
+pred_times$median[i]=MEDIAN_pred
+pred_times$real_peak[i]=xcoord_test_pred
+pred_times$Date[i]=as.character(prediction_date)
 pl <-
   plot_ly(
     mode = 'lines+markers'
@@ -208,7 +231,7 @@ pl <-
     y =  ~ 58000,
     mode = 'markers',
     type = 'scatter',
-    name = paste("MEDIAN of models Predicted Daily Peak",MEDIAN_pred),
+    name = paste("Mean of peak time of models Predicted Daily Peak",MEDIAN_pred),
     marker = list(color = ("orange"),size=9,symbol="circle")
   )%>%
   add_trace(
@@ -229,7 +252,7 @@ pl <-
     yaxis = list(title = "Power Consumption")
   )
 pl
-output=paste("G:/degree project/OUTPUT/test/",prediction_date,".html", sep="")
+output=paste("G:/degree project/OUTPUT/test2/",prediction_date,".html", sep="")
 htmlwidgets::saveWidget(pl, output)
 i=i+1;
 }
